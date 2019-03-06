0fd2307dc86f143207156adf22205157212c0a25
Infection2k <48267177+Infection2k@users.noreply.github.com>
2019-03-05 18:15:17 -0800

    STart

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello Declan")
+var num1 = 98
+console.log(num1)
+console.log(89)
+console.log(num1 + 1)
+var num3 = 50
+var num2 = 56
+var total = (num2 + num3)
+console.log(total)
+
+function Favgames(){
+  document.getElementById("my poop").src = 'http://i.imgur.com/DMahcjr.jpg';
+  document.getElementById("changetext").innerHTML = "Long Neck The Fish";
+}
+
+function Videogames(){
+  document.getElementById("my poop").src = 'https://cdn2.scratch.mit.edu/get_image/user/26711394_60x60.png';
+  document.getElementById("changetext").innerHTML = 'Long Nose The Sponge';
+}
+
+function Liteshow(){
+  document.getElementById("my poop").src = 'https://s3.dexerto.com/thumbnails/_thumbnailLarge/x-4-storwming-major-change-coming.jpg';
+  document.getElementById("changetext").innerHTML = 'Patrick Loves Planes BTW and Streamer BTWs';
+}
+// dnawjdjansjdnwaj//
